7bff3ddf242337765542c866453bfe9fe6456537
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2022-04-05 16:32:58 -0500

    first commit

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const routesFile = require("./routes/routesFile")
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors());
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
